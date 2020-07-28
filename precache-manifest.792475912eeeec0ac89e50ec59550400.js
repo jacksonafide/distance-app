ed1a058339fbb00601ef4511467201e4fa530e2b
Jacek Chojnacki <jacek.0411@gmail.com>
2020-07-28 20:22:54 +0200

    Updates

---
+++ precache-manifest.792475912eeeec0ac89e50ec59550400.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd16686bfc2feb227066c934be619b39",
+    "url": "/distance-app/index.html"
+  },
+  {
+    "revision": "686c5e0108730c4a2639",
+    "url": "/distance-app/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "427d7aac604d6fc226aa",
+    "url": "/distance-app/static/css/main.5ab2f3d2.chunk.css"
+  },
+  {
+    "revision": "686c5e0108730c4a2639",
+    "url": "/distance-app/static/js/2.f4aebf92.chunk.js"
+  },
+  {
+    "revision": "08bbb84541b4de554a51261551c7aaf4",
+    "url": "/distance-app/static/js/2.f4aebf92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "427d7aac604d6fc226aa",
+    "url": "/distance-app/static/js/main.2946c488.chunk.js"
+  },
+  {
+    "revision": "62e2101ab17f642ef884",
+    "url": "/distance-app/static/js/runtime-main.11810269.js"
+  },
+  {
+    "revision": "2ceaf661f7c099123415f94f97e2a0c5",
+    "url": "/distance-app/static/media/location_icon.2ceaf661.svg"
+  }
+]);
