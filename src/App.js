c45e0dcec1cf38039560fb8171b4305251ffe04f
Jacek Chojnacki <jacek.0411@gmail.com>
2020-07-28 23:28:30 +0200

    added logo

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import Geocode from './components/Geocode/Geocode';
 import './App.css';
 import classes from './App.module.css';
+import logo from './assets/logo.png';
 
 function App() {
   return (
     <div className="App">
       <header className = {classes.Header}>
-        Distance between cities
+        <p className = {classes.Title}><img className = {classes.Logo} src = {logo} alt = "logo" height = "42" width = "50"/>Distance between cities</p>
       </header>
       <main className = {classes.Main}>
         <Geocode />
